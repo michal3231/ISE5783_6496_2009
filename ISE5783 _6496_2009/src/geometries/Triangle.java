941cf04a3aa9e0d9c3f8bfd7ee8d4e401bef5dfb
michal <yisascha@g.jct.ac.il>
2023-03-29 18:56:20 +0300

    add triangle

---
+++ ISE5783 _6496_2009/src/geometries/Triangle.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package geometries;
+
+/**
+ * @author USER
+ *
+ */
+public class Triangle extends Polygon {
+
+	/**
+	 * 
+	 */
+	public Triangle() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
